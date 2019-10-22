e49469e6b98bf80e08422d054a647796383dbb96
12345puja <56876517+12345puja@users.noreply.github.com>
2019-10-22 18:42:08 +0530

    Add files via upload

---
+++ decimal to binary.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,j,n,r=0,a[100],d;
+	printf("enter a decimal no.:");
+	scanf("%d",&n);
+	while(n>=0)
+	{
+		r=n%2;
+		a[i]++;
+		
+		d=d/2;
+	}
+	for(j=i;j>i-1;j--)
+	printf("%d",a[j]);
+}
